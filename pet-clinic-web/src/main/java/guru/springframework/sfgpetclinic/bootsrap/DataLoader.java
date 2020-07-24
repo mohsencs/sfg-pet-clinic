21417f7a4ac5cd5e67c75185100a393fc3451867
sm <m@gmail.com>
2020-07-24 14:26:19 +0430

    Add spring data jpa service for vets. closes #41

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootsrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootsrap/DataLoader.java
@@ -95,14 +95,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Same");
         vet1.setLastName("Axe");
-        vet1.getSpiSpecialities().add(savedRadiology);
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-        vet2.getSpiSpecialities().add(savedSurgery);
+        vet2.getSpecialities().add(savedSurgery);
 
         vetService.save(vet2);
 
